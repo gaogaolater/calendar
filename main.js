68b69c15807111ab7e8e6867f2b45a0e8622cc18
gaofeng32 <gaofeng18@jd.com>
2025-02-23 10:16:51 +0800

    feat: 优化macos上的icon

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
-const { app, BrowserWindow, Tray, nativeImage, screen } = require("electron");
+const {
+  app,
+  BrowserWindow,
+  Tray,
+  nativeImage,
+  screen,
+  Menu,
+} = require("electron");
 const path = require("path");
 
 let blurTimer = null;
@@ -9,6 +16,9 @@ const createWindow = () => {
   const x = width - 350; // 设置窗口的 x 坐标为屏幕宽度减去窗口宽度
   const y = height - 358; // 设置窗口的 y 坐标为屏幕高度减去窗口高度
 
+  const iconPath = path.join(__dirname, "material", "calendar.png"); // 托盘图标路径
+  const icon = nativeImage.createFromPath(iconPath);
+
   const win = new BrowserWindow({
     width: 350,
     height: 358,
@@ -16,6 +26,8 @@ const createWindow = () => {
     y: y,
     resizable: false, // 禁止窗口大小调整
     frame: false, // 无边框窗口
+    icon: icon,
+    show: false,
   });
 
   win.loadFile("one.html");
@@ -30,19 +42,51 @@ const createWindow = () => {
     }
   });
 
-  const iconPath = path.join(__dirname, "material", "calendar.png"); // 托盘图标路径
-  const icon = nativeImage.createFromPath(iconPath);
   const tray = new Tray(icon);
+  tray.setImage(icon);
+
   // 监听托盘图标的点击事件
   tray.on("click", () => {
     if (win.isVisible()) {
       win.hide(); // 如果窗口可见，则隐藏
     } else {
-      win.show(); // 如果窗口隐藏，则显示
+      showWindowUnderTray(tray, win);
     }
   });
 };
 
+function showWindowUnderTray(tray, mainWindow) {
+  if (!mainWindow) return;
+
+  // 获取 Tray 图标的位置
+  const trayBounds = tray.getBounds();
+
+  // 获取主窗口的大小
+  const windowBounds = mainWindow.getBounds();
+
+  // 获取当前屏幕的尺寸
+  const screenSize = screen.getPrimaryDisplay().workAreaSize;
+
+  // 计算窗口的位置
+  let x = Math.round(
+    trayBounds.x - (windowBounds.width - trayBounds.width) / 2
+  );
+  let y = trayBounds.y + trayBounds.height;
+
+  // 确保窗口不会超出屏幕
+  if (x + windowBounds.width > screenSize.width) {
+    x = screenSize.width - windowBounds.width;
+  }
+  if (y + windowBounds.height > screenSize.height) {
+    y = trayBounds.y - windowBounds.height;
+  }
+
+  // 设置窗口位置并显示窗口
+  mainWindow.setPosition(x, y);
+  mainWindow.show();
+}
+
 app.whenReady().then(() => {
+  app.dock.hide();
   createWindow();
 });
